2ccac30bcc7d15ef518e8f250a8a9945c0cf9f65
VanillaBean123 <jodiemicallef4@gmail.com>
2024-05-15 11:31:08 +0200

    wed 15th
    obstacles
    collisions
    player flip
    gameover scene

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -6,23 +6,37 @@ public class PlayerMove : MonoBehaviour
 {
     public float playerSpeed;
     private Rigidbody2D rb;
+    private SpriteRenderer sr;
     private Vector2 playerDirection;
+    private bool isGrounded;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
-        if (Input.GetButtonDown("Jump") == true)
+        if (Input.GetButtonDown("Jump") == true && isGrounded == true)
         {
             rb.gravityScale *= -1;
+            sr.flipY = !sr.flipY;
         }
     }
 
-    void FixedUpdate()
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.collider.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
     }
 }
